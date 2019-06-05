69c2ad3b33db558587c4f8e16c54be8890a5d1ba
Amir Farahani <farahaniamir08@gmail.com>
2019-06-05 15:16:35 +0430

    sysupgrade modified and git_time become becutiful

--- package/base-files/version.sh
+++ package/base-files/version.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+rm -rf /home/amir/Project/Modem/Raytronik-Modem-MTK7628-3.10.14/bin/ramips/*-sysupgrade.bin
 git rev-parse --short=5 HEAD > /home/amir/Project/Modem/Raytronik-Modem-MTK7628-3.10.14/package/base-files/files/etc/git_date_version
-date '+%D -- %R'>> /home/amir/Project/Modem/Raytronik-Modem-MTK7628-3.10.14/package/base-files/files/etc/git_date_version
+date +"- %F - %T" >> /home/amir/Project/Modem/Raytronik-Modem-MTK7628-3.10.14/package/base-files/files/etc/git_date_version
